def66403803f1d5bf96b47a084111514236faf7f
Peterbrian254 <peterbrian8957@gmail.com>
2023-05-01 14:07:51 +0000

    new

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,29 +1,26 @@
 #!/usr/bin/python3
-
-"""index file for app"""
-
+"""Module for app_views /status route"""
 from api.v1.views import app_views
 from flask import jsonify
 from models import storage
 
 
-@app_views.route('/status', strict_slashes=False)
-def api_status():
-    """return api status"""
-
+@app_views.route('/status', methods=["GET"])
+def status():
+    """Return /status api route"""
     return jsonify({"status": "OK"})
 
 
-@app_views.route('/stats', strict_slashes=False)
-def obj_stats():
-    """retrieves the number of each object by type"""
-
-    my_dict = {
-            "amenities": storage.count("Amenity"),
-            "cities": storage.count("City"),
-            "places": storage.count("Place"),
-            "reviews": storage.count("Review"),
-            "states": storage.count("State"),
-            "users": storage.count("User")
-            }
-    return jsonify(my_dict)
+@app_views.route('/stats', methods=["GET"])
+def stats():
+    """Return /status api route"""
+    d = {
+        "amenities": "Amenity",
+        "cities": "City",
+        "places": "Place",
+        "reviews": "Review",
+        "states": "State",
+        "users": "User"
+    }
+    d = {k: storage.count(v) for k, v in d.items()}
+    return jsonify(d)
