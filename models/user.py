fdeeb5c72496fd369a3dc578c379be65325909ad
Caroline <carolinekimatu6@gmail.com>
2023-05-01 09:01:43 -0700

    task 14

--- models/user.py
+++ models/user.py
@@ -27,3 +27,9 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+    def __setattr_(self, k, v):
+        """sets password"""
+        if k == "password":
+            v == hashlib.md5(v.encode()).hexdigest()
+        super().__setattr__(k, v)
